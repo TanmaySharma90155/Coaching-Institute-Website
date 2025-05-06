8d6ca95153af6ca148b9d2b885520509026fe7b6
Tanmay Sharma <tanmaysharma90155@gmail.com>
2025-05-06 15:19:29 +0530

    Assignments

--- src/components/assign/Assign.jsx
+++ src/components/assign/Assign.jsx
@@ -1,15 +1,48 @@
 import React from "react";
 import "./assign.css";
+import Logo from "../../assets/unnamed.jpg";
 
 const Assign = () => {
     return (
-        <div className="assignments">
-            Hello World
-            <iframe 
-                 
-                title="Google Drive Assignments" // Add a descriptive title here
-                style={{ width: '100%', height: '500px', border: 'none' }} // Optional: Add styles for better presentation
-            ></iframe>
+        <div className="assign__container" id="assignments">
+            <div className="assign__title"> 
+                <img src={Logo} alt="Virmani Tutorials Logo" className="logo__image" />  
+                <span className="title__text">Virmani Tutorials</span>
+            </div>
+            
+            <div className="assign__head">
+                Assignments
+            </div>
+            
+            <div className="assign__classes">
+                <div className="assign_center">
+                    <a href="">CLASS 6</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 7</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 8</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 9</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 10</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 11</a>
+                </div>
+                
+                <div className="assign_center">
+                    <a href="">CLASS 12</a>
+                </div>
+            </div>
         </div>
     );
 };
